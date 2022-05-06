545d2aa44f1d41024faf5533fca527fa814553a5
noemipintaldi <noemipintaldi@gmail.com>
2022-05-06 20:51:16 +0200

    snack 2 completato

--- index.php
+++ index.php
@@ -1,16 +1,10 @@
-/*
-Snack 1
-
+<!--
+##Snack 1
 1) Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
 2) Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa
 e punti fatti dalla squadra ospite. 
 3) Stampiamo a schermo tutte le partite con questo schema.
-
-Olimpia Milano - Cantù | 55-60
-
-
-*/ 
-
+-->
 
 <?php
 
@@ -62,3 +56,86 @@ $matches = [
     </p>
 </body>
 </html>
+
+
+<!--
+## Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+-->
+
+<?php
+
+$name = (!empty( $_GET['name'])) ? $_GET['name'] : '';
+$email = (!empty( $_GET['email'])) ? $_GET['email'] : '';
+$age = (!empty( $_GET['age'])) ? $_GET['age'] : '';
+
+$validazioneName = false;
+$validazioneEmail = false;
+$validazioneAge = false;
+
+//che name sia più lungo di 3 caratteri
+if (strlen($name) < 3) {
+    echo "<p>Il nome deve contenere almeno 3 caratteri";
+}else {
+    $validazioneName = true;
+}
+
+//che mail contenga un punto e una chiocciola
+if (strpos($email,'@') == 0 || strpos($email,'.') == 0) {
+    echo "<p>L'email deve contenere @ e . </p>";
+}else {
+    $validazioneEmail = true;
+}
+
+//age sia un numero
+if (!is_numeric($age)) {
+    echo "<p>Il valore dell'età deve essere un numero</p>";
+}else {
+    $validazioneAge = true;
+}
+
+if ($validazioneName && $validazioneEmail && $validazioneAge ) {
+    echo "<p>Accesso riuscito</p>";
+} else {
+    echo "<p>Accesso negato</p>";
+}
+
+?>
+
+<style>
+    
+</style>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+<body>
+    <form method="GET">
+        <div>
+            <label for="">Name</label>
+            <input style="margin-left: 15px;" type="text" name="name">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input style="margin-left: 15px;" type="text" name="email">
+        </div>
+        <div>
+            <label for="">Age</label>
+            <input style="margin-left: 15px;" type="text" name="age">
+        </div>
+        <button style="margin-top: 15px;" type="submit">Invia</button>
+    </form>
+</body>
+</html>
+
+<!--
+## Snack 4
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+## Snack 7
+Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+-->
